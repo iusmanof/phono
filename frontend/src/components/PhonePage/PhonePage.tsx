631b65eb551cc69f3f5c7ec5cb70f7a53da6fa2e
iusmanof <iusmanof@gmail.com>
2022-12-29 17:02:54 +0300

    feat: implement data phones

--- frontend/src/components/PhonePage/PhonePage.tsx
+++ frontend/src/components/PhonePage/PhonePage.tsx
@@ -1,8 +1,11 @@
 import React from 'react'
 
-function PhonePage () {
-    return(
-        <>PhonePage</>
+function PhonePage() {
+    return (
+        <div>
+            <h1>PhonePage</h1>
+            <img src="https://res.cloudinary.com/dxedgvxdu/image/upload/v1672321790/phono/mobile_affusa.jpg" alt="mobile" />
+        </div>
     )
 }
 
---
+++ frontend/src/redux/data.ts
@@ -0,0 +1,31 @@
+let phones = [
+    {
+        id: 1000,
+        inches: 6.20,
+        price: "$701.00",
+        color: "red",
+        type: "Android Phone",
+        raiting: 4.3,
+        urlImage: "https://res.cloudinary.com/dxedgvxdu/image/upload/v1672321789/phono/mobile_dhbboj.png"
+    },
+    {
+        id: 1001,
+        inches: 6.22,
+        price: "$101.00",
+        color: "blue",
+        type: "iPhone",
+        raiting: 4.8,
+        urlImage: "https://res.cloudinary.com/dxedgvxdu/image/upload/v1672321789/phono/mobile_dhbboj.png"
+    },
+    {
+        id: 1002,
+        inches: 3.20,
+        price: "$51.00",
+        color: "black",
+        type: "Smartphone",
+        raiting: 3.3,
+        urlImage: "https://res.cloudinary.com/dxedgvxdu/image/upload/v1672321789/phono/mobile_dhbboj.png"
+    }
+]
+
+export {phones}
